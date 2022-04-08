5b3da34032963b36a32f6047b4f6ba29003e5ae2
neven kusturica <neven.kusturica@gmail.com>
2022-04-09 00:25:41 +0200

    Added ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,45 @@
+var collection = {
+    2548: {
+        album: "Slippery When Wet",
+        artist: "Bon Jovi",
+        tracks: [
+            "Let It Rock",
+            "You Give Love a Bad Name"
+        ]
+    },
+    2468 : {
+        album: "1999",
+        artist: "Prince",
+        tracks: [
+            "1999",
+            "Little Red Corvette"
+        ]
+    },
+    1245: {
+        artist: "Robert Palmer",
+        tracks: []
+    },
+    5439: {
+        album: "ABBA Gold"
+    }
+};
+
+var oldCollection = JSON.parse(JSON.stringify(collection));
+
+function updateRecords(object, id, prop, value) {
+    if (value === "") {
+        delete collection[id][prop];
+      } else if (prop === "tracks") {
+       var object = collection[id][prop] = collection[id][prop] || [];
+        object.push(value);
+      } else {
+        collection[id][prop] = value;
+      }
+      
+      return object;
+}
+console.log(updateRecords(collection, 5439, "artist", "ABBA"));
+console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
+console.log(updateRecords(collection, 2548, "artist", ""));
+console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
+module.exports = updateRecords;
